83ded87fb4a63e1fc02523ba0728be495c3ab377
ArthDias06 <arthurcastrodias@gmail.com>
2025-12-08 15:40:18 -0300

    Inserção do histórico

---
+++ TADs/headers/historico.h
@@ -0,0 +1,23 @@
+
+#ifndef HISTORICO_H
+    #define HISTORICO_H
+    #include<stdbool.h>
+    #include<stdlib.h>
+
+    typedef struct historico_ HISTORICO;
+    typedef struct procedimento_ PROCEDIMENTO;
+
+    HISTORICO *historico_criar();
+    bool historico_apagar(HISTORICO **);
+    bool historico_inserir(HISTORICO *, char *);
+    bool historico_retirar(HISTORICO *);
+    bool historico_cheio(HISTORICO *);
+    char **historico_consultar_procedimento(HISTORICO *);
+    bool historico_vazio(HISTORICO *);
+    int historico_getquantidade(HISTORICO *);
+    PROCEDIMENTO *historico_getultimo(HISTORICO *);
+    PROCEDIMENTO *procedimento_getanterior(PROCEDIMENTO *);
+    char *procedimento_gettexto(PROCEDIMENTO *);
+    void historico_inverter(HISTORICO *);
+
+#endif
